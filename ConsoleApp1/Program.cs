b83291244819e721fc3057a5e676b083ef25c69e
Jack <582597979@qq.com>
2017-06-07 22:49:45 +0800

    release version

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using ClassLibrary2;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassLibrary1.wrapper help;
+            help = new ClassLibrary1.wrapper();
+            ClassLibrary2.wrapper search;
+            search = new ClassLibrary2.wrapper();
+
+        }
+    }
+}
